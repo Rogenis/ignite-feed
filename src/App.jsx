e7996b18155c6c6044de673128271a1e53ba4534
Rogenis Silva <rps@icomp.ufam.edu.br>
2023-05-02 09:54:55 -0400

    feat: Componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import {Post} from './Post';
+import { Header } from './components/Header';
+
+import './global.css'
 
 export function App() {
   return (
     <>
-    <Post name={'Titulo'}/>
-    <Post name={'Titulo'}/>
-    <Post name={'Titulo'}/>
-    <Post name={'Titulo'}/>
-    <Post name={'Titulo'}/>
+    <Header />
+    <Post
+      author={'Rógenis Silva'}
+      content={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti blanditiis repellat accusantium culpa tempora reprehenderit necessitatibus omnis quo earum totam. Mollitia sed asperiores expedita laudantium voluptates, amet repellendus sint quam.'}
+    />
     </>
   )
 }
