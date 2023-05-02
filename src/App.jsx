2604be796fa64a25feb7b16a1f1a6d58c3341613
Rogenis Silva <rps@icomp.ufam.edu.br>
2023-05-02 14:12:33 -0400

    feat: cria componente Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import {Post} from './Post';
 import { Header } from './components/Header';
 
+import styles from './App.module.css';
 import './global.css'
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
-    <>
-    <Header />
-    <Post
-      author={'Rógenis Silva'}
-      content={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti blanditiis repellat accusantium culpa tempora reprehenderit necessitatibus omnis quo earum totam. Mollitia sed asperiores expedita laudantium voluptates, amet repellendus sint quam.'}
-    />
-    </>
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author={'Rógenis Silva'}
+            content={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti blanditiis repellat accusantium culpa tempora reprehenderit necessitatibus omnis quo earum totam. Mollitia sed asperiores expedita laudantium voluptates, amet repellendus sint quam.'}
+          />
+        </main>
+      </div>
+    </div>
   )
 }
